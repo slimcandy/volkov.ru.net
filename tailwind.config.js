c7f68a34b99e3ab15dc8d46e4465a4d2498222e2
John Doe <johndoe@example.com>
2025-09-15 16:56:26 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "text-1": "#313031",
+        "text-2": "#6a686f",
+        ad6eff: "#ad6eff",
+        "7a37d2": "#7a37d2",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        base18: "18px",
+        lg: "20px",
+        l: "28px",
+        xl: "40px",
+      },
+    },
+  },
+  plugins: [],
+};
